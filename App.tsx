8ebdb35283566c469c3df4ac47130da053c39bea
alfred <alfred@phoenixtechnologies.hk>
2024-05-31 07:22:22 +0600

    init

--- App.tsx
+++ App.tsx
@@ -6,55 +6,133 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
+  FlatList,
   SafeAreaView,
-  ScrollView,
   StatusBar,
-  StyleSheet,
   Text,
   useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
+const quranChapters = [
+  'Al-Fatiha',
+  'Al-Baqarah',
+  'Aali Imran',
+  'An-Nisa',
+  "Al-Ma'idah",
+  "Al-An'am",
+  "Al-A'raf",
+  'Al-Anfal',
+  'At-Tawbah',
+  'Yunus',
+  'Hud',
+  'Yusuf',
+  "Ar-Ra'd",
+  'Ibrahim',
+  'Al-Hijr',
+  'An-Nahl',
+  'Al-Isra',
+  'Al-Kahf',
+  'Maryam',
+  'Taha',
+  'Al-Anbiya',
+  'Al-Hajj',
+  'Al-Mu’minun',
+  'An-Nur',
+  'Al-Furqan',
+  "Ash-Shu'ara",
+  'An-Naml',
+  'Al-Qasas',
+  'Al-Ankabut',
+  'Ar-Rum',
+  'Luqman',
+  'As-Sajda',
+  'Al-Ahzab',
+  'Saba',
+  'Fatir',
+  'Yasin',
+  'As-Saffat',
+  'Sad',
+  'Az-Zumar',
+  'Ghafir',
+  'Fussilat',
+  'Ash-Shura',
+  'Az-Zukhruf',
+  'Ad-Dukhan',
+  'Al-Jathiya',
+  'Al-Ahqaf',
+  'Muhammad',
+  'Al-Fath',
+  'Al-Hujurat',
+  'Qaf',
+  'Adh-Dhariyat',
+  'At-Tur',
+  'An-Najm',
+  'Al-Qamar',
+  'Ar-Rahman',
+  "Al-Waqi'a",
+  'Al-Hadid',
+  'Al-Mujadila',
+  'Al-Hashr',
+  'Al-Mumtahina',
+  'As-Saff',
+  'Al-Jumua',
+  'Al-Munafiqun',
+  'At-Taghabun',
+  'At-Talaq',
+  'At-Tahrim',
+  'Al-Mulk',
+  'Al-Qalam',
+  'Al-Haqqah',
+  "Al-Ma'arij",
+  'Nuh',
+  'Al-Jinn',
+  'Al-Muzzammil',
+  'Al-Muddaththir',
+  'Al-Qiyamah',
+  'Al-Insan',
+  'Al-Mursalat',
+  'An-Naba',
+  "An-Nazi'at",
+  'Abasa',
+  'At-Takwir',
+  'Al-Infitar',
+  'Al-Mutaffifin',
+  'Al-Inshiqaq',
+  'Al-Buruj',
+  'At-Tariq',
+  "Al-A'la",
+  'Al-Ghashiya',
+  'Al-Fajr',
+  'Al-Balad',
+  'Ash-Shams',
+  'Al-Layl',
+  'Ad-Duha',
+  'Ash-Sharh',
+  'At-Tin',
+  'Al-Alaq',
+  'Al-Qadr',
+  'Al-Bayyina',
+  'Az-Zalzalah',
+  'Al-Adiyat',
+  "Al-Qari'a",
+  'At-Takathur',
+  'Al-Asr',
+  'Al-Humaza',
+  'Al-Fil',
+  'Quraysh',
+  "Al-Ma'un",
+  'Al-Kawthar',
+  'Al-Kafirun',
+  'An-Nasr',
+  'Al-Masad',
+  'Al-Ikhlas',
+  'Al-Falaq',
+  'An-Nas',
+];
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
@@ -63,56 +141,28 @@ function App(): React.JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={backgroundStyle} className="flex">
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <View className="flex items-center justify-center h-[40px] mb-0.5">
+        <Text className="text-2xl text-center">Holy Quran</Text>
+      </View>
+
+      <View className="bg-gray-50 mb-0.5" style={{height: '93%'}}>
+        <FlatList
+          data={quranChapters}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({item}) => (
+            <View className="bg-gray-200 p-6 my-0.5">
+              <Text className="text-lg">{item}</Text>
+            </View>
+          )}
+        />
+      </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
